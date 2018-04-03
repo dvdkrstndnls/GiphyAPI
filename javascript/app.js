d73e61af15524c3404b30cfc04a3357337d10309
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-04-03 08:14:21 -0500

    html : moved gifs below buttons

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 // 1. Create an array of strings, each one related to a topic. Saved to a variable called `topics`.
 //defined variables here
-var topicsArray = ['wakanda', 'little kids dancing', 'dancing bloopers', 'african tribal dancing', 'pop and lock', 'hip hop dancing', 'ballroom dancing', 'capoiera', 'hippies dancing', 'old people dancing',];
+var topicsArray = ['wakanda', 'little kids dancing', 'dancing bloopers', 'african tribal dancing', 'pop and lock', 'hip hop dancing', 'ballroom dancing', 'polka', 'hippies dancing', 'old people dancing',];
 //create arraylenth to be able to iterate through topicsArray (do we need this?)
 var arrayLength = topicsArray.length;
 // var i = 0;
@@ -83,20 +83,20 @@ displayButtons();
 //  <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
 //  <img src="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-still="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-animate="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200.gif" data-state="still" class="gif">
 //  <img src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif" data-state="still" class="gif">
-//    $(".gif").on("click", function() {
-//      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-//      var state = $(this).attr("data-state");
-//      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-//      // Then, set the image's data-state to animate
-//      // Else set src to the data-still value
-//      if (state === "still") {
-//        $(this).attr("src", $(this).attr("data-animate"));
-//        $(this).attr("data-state", "animate");
-//      } else {
-//        $(this).attr("src", $(this).attr("data-still"));
-//        $(this).attr("data-state", "still");
-//      }
-//    });
+   $(".gif").on("click", function() {
+     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+     var state = $(this).attr("data-state");
+     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+     // Then, set the image's data-state to animate
+     // Else set src to the data-still value
+     if (state === "still") {
+       $(this).attr("src", $(this).attr("data-animate"));
+       $(this).attr("data-state", "animate");
+     } else {
+       $(this).attr("src", $(this).attr("data-still"));
+       $(this).attr("data-state", "still");
+     }
+   });
 
 
 // 5. Under every gif, display its rating (PG, G, so on).
